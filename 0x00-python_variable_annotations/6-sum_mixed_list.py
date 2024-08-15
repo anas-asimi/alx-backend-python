76febcdf1c2e0ff8f2c239b1b9d7f54267f83d6a
Anas Asimi <anas2020cr7@gmail.com>
2024-08-15 13:28:02 +0100

    fix task 6

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -3,8 +3,10 @@
 6-sum_mixed_list.py
 """
 
+from typing import Union, List
 
-def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """_summary_
 
     Args:

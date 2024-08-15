a2870329e579ded23f3843576318dc5a37cab103
Anas Asimi <anas2020cr7@gmail.com>
2024-08-15 12:37:03 +0100

    task 1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+1-concat.py
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """_summary_
+
+    Args:
+        str1 (str): _description_
+        str2 (str): _description_
+
+    Returns:
+        str: _description_
+    """
+    return str1+str2

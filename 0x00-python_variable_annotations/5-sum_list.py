532fc8c8216d1a16f07cb73ca238978a5f3d76f8
Anas Asimi <anas2020cr7@gmail.com>
2024-08-15 13:17:45 +0100

    add documentation

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,6 +5,14 @@
 
 
 def sum_list(input_list: list[float]) -> float:
+    """_summary_
+
+    Args:
+        input_list (list[float]): _description_
+
+    Returns:
+        float: _description_
+    """
     n: float = 0
     for x in input_list:
         n += x
--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -5,6 +5,14 @@
 
 
 def sum_mixed_list(mxd_lst: list[int, float]) -> float:
+    """_summary_
+
+    Args:
+        mxd_lst (list[int, float]): _description_
+
+    Returns:
+        float: _description_
+    """
     n: float = 0
     for x in mxd_lst:
         n += x

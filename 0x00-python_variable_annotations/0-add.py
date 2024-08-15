74589deae4f2bc399328dc3cf54ac09d6fd0c7e8
Anas Asimi <anas2020cr7@gmail.com>
2024-08-15 12:33:27 +0100

    task 0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+0-add.py
+"""
+
+
+def add(a: float, b: float) -> float:
+    """_summary_
+    Args:
+        a (float): _description_
+        b (float): _description_
+    Returns:
+        float: _description_
+    """
+    return a+b

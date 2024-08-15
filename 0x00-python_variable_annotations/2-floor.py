888d658f23b395b28a8884bef6103590dd8d565d
Anas Asimi <anas2020cr7@gmail.com>
2024-08-15 12:38:49 +0100

    task 2. Basic annotations - floor

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+2-floor.py
+"""
+
+
+def floor(n: float) -> int:
+    """_summary_
+
+    Args:
+        n (float): _description_
+
+    Returns:
+        int: _description_
+    """
+    return int(n)
